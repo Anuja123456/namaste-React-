adcf4d9f5076d326b53351b16828987228a3b585
anuja123456 <anujabirnale@gmail.com>
2025-02-04 23:43:10 +0530

    episode_4

--- App.js
+++ App.js
@@ -1,7 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading = React.createElement("h1",{id:"heading"},"My first heading");
+
+const Title =()=>{
+    return(<h1>Anuja</h1>)
+}
+
+// component composition
+const HeadingComponent = () =>(
+   <div id="container">
+        <Title/>
+        <h1>Anuja is working in IBM</h1>
+    </div>
+)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(heading);
+root.render(<HeadingComponent />);
